d90e46e5b1b79ad0f07dd84fde528a876d6d6881
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-03 07:37:34 +0200

    Apply mechanical lint fixes (swift-format, SwiftLint)

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .macOS(.v26),
         .iOS(.v26),
         .tvOS(.v26),
-        .watchOS(.v26)
+        .watchOS(.v26),
     ],
     products: [
         .library(
@@ -26,23 +26,32 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-ietf/swift-rfc-5234.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            branch: "main"
+        ),
+        .package(url: "https://github.com/swift-ietf/swift-rfc-5234.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "RFC 7405",
             dependencies: [
                 .product(name: "RFC 5234", package: "swift-rfc-5234"),
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
-                .product(name: "ASCII Primitives", package: "swift-ascii-primitives")
-    ]
+                .product(
+                    name: "Standard Library Extensions",
+                    package: "swift-standard-library-extensions"
+                ),
+                .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
+            ]
         ),
         .testTarget(
             name: "RFC 7405 Tests",
             dependencies: [
-                "RFC 7405",
+                "RFC 7405"
             ]
         ),
     ],
